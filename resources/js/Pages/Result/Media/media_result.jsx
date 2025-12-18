d0f120820f811dcfa4ecc9bdefdbc5e9f0b76f2e
Vicky Emeleo Putra <vicky@example.com>
2025-12-18 09:20:12 +0700

    `Updated date formatting and added time display in task result pages`

--- resources/js/Pages/Result/Media/media_result.jsx
+++ resources/js/Pages/Result/Media/media_result.jsx
@@ -503,6 +503,7 @@ export default function TaskResult({ tasks, users, userName }) {
                                         </h4>
                                         <p className="text-sm text-gray-600 dark:text-gray-400">
                                             {selectedTask.send_date}
+                                            {selectedTask.send_time}
                                         </p>
                                     </div>
                                 </div>
